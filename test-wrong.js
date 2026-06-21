85f7019a8bc7ae293340f18b924690672f0c9d41
binesh7543 <dhg77744@gmail.com>
2026-06-21 19:09:08 +0530

    Correct syntax error in test-wrong.js
    Fix missing closing bracket in fetch function.

---
+++ test-wrong.js
@@ -0,0 +1,7 @@
+// Intentional syntax error
+export default {
+  async fetch(request, env) {
+    return new Response('OK'
+  // यहाँ ब्रैकेट की कमी है (missing closing bracket)
+                        }
+  
